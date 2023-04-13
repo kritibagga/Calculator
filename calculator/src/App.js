ff29b994423579cf09d402995dffd3475fae5fa9
kriti bagga <bagga.kriti@gmail.com>
2023-04-13 17:17:59 -0400

    Basic Layout

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
+import Wrapper from "./Components/Wrapper.js";
+import Screen from "./Components/Screen";
+import ButtonBox from "./Components/ButtonBox";
+import Button from "./Components/Button";
 
+const btnValues = [
+	["C", "+-", "%", "/"],
+	[7, 8, 9, "X"],
+	[4, 5, 6, "-"],
+	[1, 2, 3, "+"],
+	[0, ".", "="],
+];
 function App() {
-	return <h1>hello world</h1>;
+	return (
+		<>
+    <h1 className="heading">Calculator</h1>
+			<Wrapper>
+				<Screen value='0' />
+				<ButtonBox>
+					{btnValues.flat().map((btn, i) => {
+						return (
+							<Button
+								key={i}
+								className={btn === "=" ? "equals" : ""}
+								value={btn}
+								onClick={() => {
+									console.log(`${btn} clicked!`);
+								}}
+							/>
+						);
+					})}
+				</ButtonBox>
+			</Wrapper>
+		</>
+	);
 }
 
 export default App;
