95ed37e0687daf1b55a49d0ff7cac97ae5644f68
kriti bagga <bagga.kriti@gmail.com>
2023-04-14 13:01:53 -0400

    calculator has been created

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState } from "react";
 import Wrapper from "./Components/Wrapper.js";
 import Screen from "./Components/Screen";
 import ButtonBox from "./Components/ButtonBox";
@@ -11,12 +12,128 @@ const btnValues = [
 	[1, 2, 3, "+"],
 	[0, ".", "="],
 ];
-function App() {
+
+const toLocaleString = (num) =>
+	String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+const App = () => {
+	let [calc, setCalc] = useState({
+		sign: "",
+		num: 0,
+		res: 0,
+	});
+
+	//numClickHandler Function
+	const numClickHandler = (e) => {
+		e.preventDefault();
+		const value = e.target.innerHTML;
+
+		if (removeSpaces(calc.num).length < 16) {
+			setCalc({
+				...calc,
+				num:
+					calc.num === 0 && value === "0"
+						? "0"
+						: removeSpaces(calc.num) % 1 === 0
+						? toLocaleString(Number(removeSpaces(calc.num + value)))
+						: toLocaleString(calc.num + value),
+				res: !calc.sign ? 0 : calc.res,
+			});
+		}
+	};
+
+	//commaClickHandler Function
+	const commaClickHandler = (e) => {
+		e.preventDefault();
+		const value = e.target.innerHTML;
+		setCalc({
+			...calc,
+			num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+		});
+	};
+
+	//signClickHandler Function
+	const signClickHandler = (e) => {
+		e.preventDefault();
+		const value = e.target.innerHTML;
+
+		setCalc({
+			...calc,
+			sign: value,
+			res: !calc.res && calc.num ? calc.num : calc.res,
+			num: 0,
+		});
+	};
+
+	//equalClickHandler Function
+	const equalsClickHandler = () => {
+		if (calc.sign && calc.num) {
+			const math = (a, b, sign) =>
+				sign === "+"
+					? a + b
+					: sign === "-"
+					? a - b
+					: sign === "X"
+					? a * b
+					: a / b;
+
+			setCalc({
+				...calc,
+				res:
+					calc.num === 0 && calc.sign === "/"
+						? "Can't divide with zero"
+						: toLocaleString(
+								math(
+									Number(removeSpaces(calc.res)),
+									Number(removeSpaces(calc.num)),
+									calc.sign
+								)
+						  ),
+				sign: "",
+				num: 0,
+			});
+		}
+	};
+
+	//invertCickHandler Function
+	const invertClickHandler = () => {
+		setCalc({
+			...calc,
+			num: calc.num ? toLocaleString(removeSpaces(calc.num) * -1) : 0,
+			res: calc.res ? toLocaleString(removeSpaces(calc.res) * -1) : 0,
+			sign: "",
+		});
+	};
+
+	//percentageClickHandler Function
+	const percentClickHandler = () => {
+		let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+		let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
+
+		setCalc({
+			...calc,
+			num: (num /= Math.pow(100, 1)),
+			res: (res /= Math.pow(100, 1)),
+			sign: "",
+		});
+	};
+
+	//resetClickHandler Function
+	const resetClickHandler = () => {
+		setCalc({
+			...calc,
+			sign: "",
+			num: 0,
+			res: 0,
+		});
+	};
+
 	return (
 		<>
-    <h1 className="heading">Calculator</h1>
+			<h1 className='heading'>Calculator</h1>
 			<Wrapper>
-				<Screen value='0' />
+				<Screen value={calc.num ? calc.num : calc.res} />
 				<ButtonBox>
 					{btnValues.flat().map((btn, i) => {
 						return (
@@ -24,9 +141,21 @@ function App() {
 								key={i}
 								className={btn === "=" ? "equals" : ""}
 								value={btn}
-								onClick={() => {
-									console.log(`${btn} clicked!`);
-								}}
+								onClick={
+									btn === "C"
+										? resetClickHandler
+										: btn === "+-"
+										? invertClickHandler
+										: btn === "%"
+										? percentClickHandler
+										: btn === "="
+										? equalsClickHandler
+										: btn === "/" || btn === "X" || btn === "-" || btn === "+"
+										? signClickHandler
+										: btn === "."
+										? commaClickHandler
+										: numClickHandler
+								}
 							/>
 						);
 					})}
@@ -34,6 +163,6 @@ function App() {
 			</Wrapper>
 		</>
 	);
-}
+};
 
 export default App;
